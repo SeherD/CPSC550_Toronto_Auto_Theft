071956155409a67b64af04024208a9cb40a5d8e4
dhyey <dhyeylalseta@outlook.com>
2023-12-14 14:36:43 -0700

    prophet related stuff

--- toronto.py
+++ toronto.py
@@ -5,6 +5,7 @@ import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
 import csv
+import plotly.express as px
 from sklearn.cluster import KMeans
 
 data = pd.read_csv('export.csv', header=None)
@@ -25,20 +26,38 @@ data['Cluster'] = clusters
 # Count the points in each cluster
 cluster_counts = pd.Series(clusters).value_counts()
 
+
+# Spark data analysis dataframes
+neighbourhood_frequencies = pd.read_csv('spark_data/neighbourhood_freq.csv')
+weekday_frequencies = pd.read_csv('spark_data/weekday_freq.csv')
+
+neighbourhood_fig = px.bar(neighbourhood_frequencies, x='NEIGHBOURHOOD_158', y='count', title="Accident frequency per neighbourhood")
+neighbourhood_fig.update_layout(xaxis_title="Neighbourhood", yaxis_title="Accident count")
+
+weekday_fig = px.bar(weekday_frequencies, x='OCC_DOW', y='count', title="Accident frequency per weekday")
+weekday_fig.update_layout(xaxis_title="Weekday", yaxis_title="Accident count")
+
+
 app = dash.Dash(__name__)
 # Create a Dash web application
 app.layout = html.Div([
     html.H1("Toronto Cluster Map Dashboard"),
+    html.Div([
+        dcc.DatePickerRange(
+            id='date-picker-range',
+            start_date=df['Timestamp'].min(),
+            end_date=df['Timestamp'].max(),
+            display_format='YYYY-MM-DD',
+            style={'margin': '10px'}
+        ),
 
-    dcc.DatePickerRange(
-        id='date-picker-range',
-        start_date=df['Timestamp'].min(),
-        end_date=df['Timestamp'].max(),
-        display_format='YYYY-MM-DD',
-        style={'margin': '10px'}
-    ),
-
-    html.Div(id='map-container')
+        html.Div(id='map-container')    
+    ]),
+    html.H1("Toronto Accident Frequencies"),
+    html.Div([
+        dcc.Graph(figure=neighbourhood_fig),
+        dcc.Graph(figure=weekday_fig)
+    ])
 ])
 
 # Callback to update the map based on the selected date range
